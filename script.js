9cc2450c4f95a69f5eaa416d462845084ce9a989
Aneeshbs44 <anish107655@gmail.com>
2024-11-15 18:21:49 +0530

    Added few lines of JS Code

---
+++ script.js
@@ -0,0 +1,7 @@
+const gameContainer = document.querySelector(".container");
+const userResult = document.querySelector(".user_result img");
+const cpuResult = document.querySelector(".cpu_result img");
+const result = document.querySelector(".result");
+const optionImages = document.querySelector(".option_image");
+
+console.log(gameContainer, userResult, cpuResult, result, optionImages);
